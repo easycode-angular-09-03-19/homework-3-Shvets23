eea3d47106bfc7ca47190e96ee49ed5d7d38cc21
Shvets Olena <shvets.o@hexide-digital.com>
2019-03-17 00:55:45 +0200

    style app

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -11,16 +11,13 @@ export class CarComponent implements OnInit {
     mileage: 0,
     fuel: 5,
     img: 'http://pngimg.com/uploads/mini/mini_PNG11788.png',
-    specifications: ['Двигатель 2 литра', 'Максимальная скорость 200 км/ч', 'Расход топлива 10 л на 100 км']
+    specifications: ['Engine: 1.6-litre 120.7bhp Petrol Engine;', 'Top Speed: 197km/hr;', 'Fuel Tank Capacity: 40 Liters;', 'TransmissionType: Automatic;']
   }
   constructor() { }
   drive() {
     if (this.carInfo.fuel >0){
       --this.carInfo.fuel;
       this.carInfo.mileage += 10;
-    console.log(this.carInfo.fuel, this.carInfo.mileage)
-    }else if (this.carInfo.fuel == 0) {
-      alert('Not today, guy, not today...')
     }
     
   }
